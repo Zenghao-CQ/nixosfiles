8e727837f6c2a025585001e975fa0a44c0d16bf5
Hao Zeng <zenghao-cq@pku.edu.cn>
2025-01-03 13:02:51 +0800

    feat: enable vscode server

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -112,7 +112,8 @@
     # ];
   };
   programs.zsh.enable = true; 
-
+  # Enable ld to run vscode-server
+  programs.nix-ld.enable = true;
   # $ nix search wget
   environment.systemPackages = with pkgs; [
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
